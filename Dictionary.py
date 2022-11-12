8c805d5bfad5c79c52ec1831a6156cc39a1c6f56
unknown <daza.ronnie.09212002@gmail.com>
2022-11-12 15:40:01 +0800

    Final Changes

--- Dictionary.py
+++ Dictionary.py
@@ -1,4 +1,3 @@
-# Menu
 print("-----------------------------")
 print("MENU:")
 print("1 -> Add an item")
@@ -6,10 +5,12 @@ print("2 -> Search")
 print("3 -> Exit (y/n)")
 print("-----------------------------")
 
-info =  {}
+info =  {"Mark": ["19", "Manila", "09123456789"]}
 
 while True:
+
     choice = int(input("What do you want to do? (1-3) "))
+
     if choice == 1:
         fullName = input("Name: ")
         age = int(input("Age: "))
@@ -39,4 +40,3 @@ while True:
         exit = input("Are you sure? (Yes/No) ")
         if exit == "Yes":
             break
-        

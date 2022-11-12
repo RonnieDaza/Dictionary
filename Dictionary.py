a03c6b1be2d996d7ddbb877a0865bd31275b9cff
unknown <daza.ronnie.09212002@gmail.com>
2022-11-12 15:03:29 +0800

    Choice 2

--- Dictionary.py
+++ Dictionary.py
@@ -1,21 +1,36 @@
 # Menu
-print("Menu:")
+print("-----------------------------")
+print("MENU:")
 print("1 -> Add an item")
 print("2 -> Search")
 print("3 -> Exit (y/n)")
+print("-----------------------------")
 
-# Ask the user what to do.
-choice = int(input("What do you want to do? (1-3) "))
+info =  {"Mark": ["19", "Manila", "09123456789"]}
 
-if choice == 1:
-    info = {"Jiya": "Caloocan"}
+while True:
+    choice = int(input("What do you want to do? (1-3) "))
+    if choice == 1:
+        fullName = input("Name: ")
+        age = int(input("Age: "))
+        address = input("Address: ")
+        country = input("Country: ")
+        phoneNumber = input("Phone Number: ")
+        gender = input("Gender: ")
+        email = input("Email: ")
+        info[fullName] = [age, address, country, phoneNumber, gender, email]
+        print("Saved!")
     
-    name = input("Full Name: ")
-    age = int(input("Age: "))
-    address = input("Address: ")
-    number = int(input("Phone Number: "))
-    gender = input("Gender: ")
-    country = input("Country: ")
-    email = input("Email: ")
-    info[name] = [age, address, number, gender, country, email]
-    print(info)
+    elif choice == 2:
+        askName = input("Enter the full name: ")
+        print(info[askName])
+
+        userData = {
+            "Full Name": fullName,
+            "Age": age,
+            "Address": address,
+            "Country": country,
+            "Phone Number": phoneNumber,
+            "Gender": gender,
+            "Email": email
+        }

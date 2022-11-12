64ea8f0fceba0240ef13eb8c65273b2e8193a077
unknown <daza.ronnie.09212002@gmail.com>
2022-11-12 12:21:06 +0800

    Created Choice 1

--- Dictionary.py
+++ Dictionary.py
@@ -3,3 +3,17 @@ print("Menu:")
 print("1 -> Add an item")
 print("2 -> Search")
 print("3 -> Exit (y/n)")
+
+# Ask the user what to do.
+choice = int(input("What do you want to do? (1-3) "))
+
+if choice == 1:
+    fullName = input("Full Name: ")
+    age = input("Age: ")
+    gender = input("Gender: ")
+    address = input("Address: ")
+    phoneNumber = input("Phone Number: ")
+    country = input("Country: ")
+    email = input("Email: ")
+    date = input("Date: ")
+
